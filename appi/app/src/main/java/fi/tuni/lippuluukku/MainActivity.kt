73d9589ebcbc4b1447f419c14d7ab591d8d2cc62
Toni Salminen <toni.salminen89@gmail.com>
2021-05-08 14:22:24 +0300

    testing gps, api-calls and activity change / backbuttonpressed etc..

---
+++ appi/app/src/main/java/fi/tuni/lippuluukku/MainActivity.kt
@@ -0,0 +1,122 @@
+package fi.tuni.lippuluukku
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
+import com.fasterxml.jackson.databind.ObjectMapper
+
+class MainActivity : AppCompatActivity() {
+    lateinit var text : TextView
+    lateinit var button : Button
+    lateinit var activityButton : Button
+
+    var apikey : String = "wl5A0tEYNyQIQ9cTVA9VGVWlB3R8NgfO"
+    var band = "metallica"
+    var city = "tampere"
+    var radius = 30
+    var lat = 61.49911
+    var lon = 23.78712
+    var latlonString : String = "${lat},${lon}"
+
+    //request with keyword and city
+    var url1 : String = "https://app.ticketmaster.com/discovery/v2/events?apikey=${apikey}&keyword=${band}&locale=*&city=${city}"
+
+    //request with keyword
+    var url2 : String = "https://app.ticketmaster.com/discovery/v2/events?apikey=${apikey}&keyword=${band}&locale=*"
+
+    // request with city
+    var url3 : String = "https://app.ticketmaster.com/discovery/v2/events?apikey=${apikey}&locale=*&city=${city}"
+
+    //request with geopoint(lat,lon) and search radius
+    var url4 : String = "https://app.ticketmaster.com/discovery/v2/events?apikey=${apikey}&radius=${radius}&locale=*&geoPoint=${latlonString}"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        this.text = findViewById(R.id.text)
+        this.button = findViewById(R.id.button)
+        this.button.setOnClickListener(){
+            this.buttonFunc(this.button)
+        }
+        this.activityButton = findViewById(R.id.activityButton)
+        this.activityButton.setOnClickListener(){
+            this.activityButtonClick(this.activityButton)
+        }
+    }
+
+    fun activityButtonClick(button : View) {
+        val intent = Intent(this, SecondActivity::class.java)
+        intent.putExtra("testKey","testData")
+        startActivityForResult(intent,666)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 666) {
+            if(resultCode == RESULT_OK) {
+                val temp = data?.extras?.getString("second")
+                this.text.text=temp
+            }
+        }
+    }
+
+    fun buttonFunc(button : View){
+        downloadUrlAsync(this, this.url1){
+            //var resultti : String? = it
+            //val mp = ObjectMapper()
+            //val myObject: StarWarsJsonObject = mp.readValue(it, StarWarsJsonObject::class.java)
+            //val persons: MutableList<Person>? = myObject.results
+            //persons?.sortByDescending { it.getBmi(it.mass,it.height) }
+            //adapter = ArrayAdapter<Person>(this,R.layout.item,R.id.myTextView,persons!!)
+            //myListView.setAdapter(adapter)
+            //recyclerView.adapter = CustomAdapter(persons!!)
+            //Log.d("shit", resultti.toString())
+
+            //val intent = Intent(this, SecondActivity::class.java)
+
+            //intent.putExtra
+                // -> (in second activity) get extras by
+                    // val extras = intent.extras
+
+            //startActivity(intent)
+                //to get data back from second activity to mainactivity when pressing android back
+                //instead of startactivity use :
+            //startActivityForResult(intent, requestcode: 12312312)
+
+            //val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://...."))
+            //val intent = Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=jotain"))
+
+            Log.d("shit", it.toString())
+        }
+    }
+
+    fun downloadUrlAsync(context: Activity, url:String, callback:(result:String?)->Unit):Unit{
+        thread{
+            var data = getUrl(url)
+            context.runOnUiThread{
+                callback(data)
+            }
+        }
+    }
+    fun getUrl(url : String?) : String {
+        val myUrl = URL(url)
+        val conn = myUrl.openConnection() as HttpURLConnection
+        var result = ""
+        val inputstream = conn.getInputStream()
+        inputstream.use{
+            val reader = BufferedReader(InputStreamReader(inputstream))
+            result += reader.readLine()
+        }
+        return result
+    }
+}
---
+++ appi/app/src/main/java/fi/tuni/lippuluukku/SecondActivity.kt
@@ -0,0 +1,62 @@
+package fi.tuni.lippuluukku
+
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+class SecondActivity : AppCompatActivity(), LocationListener {
+
+    lateinit var text : TextView
+    lateinit var locationText : TextView
+    lateinit var button : Button
+    lateinit var locationManager : LocationManager
+    private val locationPermissionCode = 2
+
+    override fun onLocationChanged(location: Location) {
+        this.text.text = "${location.latitude}, ${location.longitude}"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        this.text = findViewById(R.id.exrasText)
+        this.locationText = findViewById(R.id.locationText)
+        this.button = findViewById(R.id.buttonSecond)
+        this.button.setOnClickListener() {
+            this.getLocation(this.button)
+        }
+        val extras = intent.extras
+        if(extras != null) {
+            text.text = extras.getString("testKey").toString()
+        }
+    }
+
+    override fun onBackPressed() {
+        val intent = Intent()
+        intent.putExtra("second","secondTest")
+        setResult(RESULT_OK, intent)
+        super.onBackPressed()
+    }
+    fun getLocation (button : View) {
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), locationPermissionCode)
+        }
+        //mintime and mindistance are how the location gets updated
+        //so for an app like this its okay if the numers are high
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5f, this)
+        //this.text.text = "homo"
+    }
+}
