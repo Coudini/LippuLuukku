ef509fb42984100e8caa66eea74c874d3af01462
Toni Salminen <toni.salminen89@gmail.com>
2021-05-11 19:39:31 +0300

    comment-out test-code. start activities on ImageButton-clicks

--- appi/app/src/main/java/fi/tuni/lippuluukku/MainActivity.kt
+++ appi/app/src/main/java/fi/tuni/lippuluukku/MainActivity.kt
@@ -6,10 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Spinner
-import android.widget.TextView
+import android.widget.*
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
@@ -19,20 +16,26 @@ import com.fasterxml.jackson.databind.ObjectMapper
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var text : TextView
+    //lateinit var text : TextView
+    lateinit var location : TextView
+    lateinit var keyword : TextView
+
+    /* test-buttons
     lateinit var button1 : Button
     lateinit var button2 : Button
     lateinit var button3 : Button
     lateinit var button4 : Button
     lateinit var button5 : Button
-    lateinit var activityButton : Button
+
+     */
+    lateinit var preferencesButton : ImageButton
+    lateinit var searchButton : ImageButton
 
     lateinit var locationSpinner : Spinner
     lateinit var keywordSpinner : Spinner
-    lateinit var keyWordText : EditText
-    lateinit var cityText : EditText
 
-    val testArray = arrayOf("one","two","three")
+//    lateinit var keyWordText : EditText
+//    lateinit var cityText : EditText
 
     var apikey : String = "wl5A0tEYNyQIQ9cTVA9VGVWlB3R8NgfO"
     var band = "metallica"
@@ -41,6 +44,9 @@ class MainActivity : AppCompatActivity() {
     var lat = 61.49911
     var lon = 23.78712
     var latlonString : String = "${lat},${lon}"
+
+    var locationsArray : Array<String> = arrayOf("Current location", "No location", "Tampere")
+    var keywordsArray : Array<String> = arrayOf("Nothing", "Metallica")
 
     //request with keyword and city
     var url1 : String = "https://app.ticketmaster.com/discovery/v2/events?apikey=${apikey}&keyword=${band}&locale=*&city=${city}"
@@ -60,7 +66,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        this.text = findViewById(R.id.text)
+        //this.text = findViewById(R.id.text)
+        /* Test-buttons
         this.button1 = findViewById(R.id.button1)
         this.button1.setOnClickListener(){
             this.buttonFunc(this.button1, url1)
@@ -81,28 +88,60 @@ class MainActivity : AppCompatActivity() {
         this.button5.setOnClickListener(){
             this.buttonFunc(this.button5, url5)
         }
-        this.activityButton = findViewById(R.id.activityButton)
-        this.activityButton.setOnClickListener(){
-            this.activityButtonClick(this.activityButton)
+         */
+
+        this.preferencesButton = findViewById(R.id.imageButtonPreferences)
+        this.preferencesButton.setOnClickListener(){
+            this.preferencesOnClick(this.preferencesButton)
         }
+
+        this.searchButton = findViewById(R.id.imageButtonSearch)
+        this.searchButton.setOnClickListener(){
+            this.searchOnClick(this.searchButton)
+        }
+
         this.locationSpinner = findViewById(R.id.locationSpinner)
+        if (this.locationSpinner != null) {
+            val adapter = ArrayAdapter(this,android.R.layout.simple_spinner_item,this.locationsArray)
+            this.locationSpinner.adapter = adapter
+        }
 
         this.keywordSpinner = findViewById(R.id.keywordSpinner)
+        if (this.keywordSpinner != null) {
+            val adapter = ArrayAdapter(this,android.R.layout.simple_spinner_item,this.keywordsArray)
+            this.keywordSpinner.adapter = adapter
+        }
 
     }
 
-    fun activityButtonClick(button : View) {
-        val intent = Intent(this, PreferencesActivity::class.java)
-        intent.putExtra("testKey","testData")
-        startActivityForResult(intent,666)
+    fun preferencesOnClick(button:View){
+        val preferencesIntent = Intent(this, PreferencesActivity::class.java)
+        preferencesIntent.putExtra("locationsArray",this.locationsArray)
+        preferencesIntent.putExtra("keywordsArray",this.keywordsArray)
+        startActivityForResult(preferencesIntent, 1)
+    }
+
+    fun searchOnClick(button:View) {
+        val resultsIntent = Intent(this, ResultsActivity::class.java)
+        resultsIntent.putExtra("searchUrl", this.url1)
+        startActivity(resultsIntent)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == 666) {
+        //preferences
+        if(requestCode == 1) {
             if(resultCode == RESULT_OK) {
-                val temp = data?.extras?.getString("second")
-                this.text.text=temp
+                val tempLocations = data?.extras?.getStringArray("locationsArray")
+                if (tempLocations != null) {
+                    this.locationsArray = tempLocations
+                }
+                val tempKeywords = data?.extras?.getStringArray("keywordsArray")
+                if (tempKeywords != null) {
+                    this.keywordsArray = tempKeywords
+                }
+            //val temp = data?.extras?.getString("second")
+                //this.text.text=temp
             }
         }
     }
@@ -117,23 +156,12 @@ class MainActivity : AppCompatActivity() {
             //adapter = ArrayAdapter<Person>(this,R.layout.item,R.id.myTextView,persons!!)
             //myListView.setAdapter(adapter)
             //recyclerView.adapter = CustomAdapter(persons!!)
-            //Log.d("shit", resultti.toString())
-
-            //val intent = Intent(this, SecondActivity::class.java)
-
-            //intent.putExtra
-                // -> (in second activity) get extras by
-                    // val extras = intent.extras
-
-            //startActivity(intent)
-                //to get data back from second activity to mainactivity when pressing android back
-                //instead of startactivity use :
-            //startActivityForResult(intent, requestcode: 12312312)
 
             //val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://...."))
             //val intent = Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=jotain"))
 
             Log.d("shit", it.toString())
+            //this.text.text = it.toString()
         }
     }
 
